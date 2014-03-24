8caaee3143e167787c9443d2e241a383b7c351fc
thor.hanks <thor.hanks@gmail.com>
2014-03-23 20:52:45 -0700

    Added Nav menu and github tab.

--- Source/js/main.js
+++ Source/js/main.js
@@ -15,6 +15,15 @@
 			$("#intro").height(viewportHeight);
 			$("#body").css("margin-top", viewportHeight + "px");
 		}
+
+		$(window).scroll(function()
+		{
+			// True if scrolled distance is into the body section
+			var isPastIntro = ($(window).scrollTop()) > ($("#body").offset().top);
+
+			$('#nav').toggleClass('sticky', isPastIntro);
+			$('#visitSource').toggleClass('sticky', isPastIntro);
+		});
 	};
 
 	var _MyPrivateFunc = function()
