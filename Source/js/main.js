877aa9c710d769f70c14b6ee830331e4f63f7fd8
thor.hanks <thor.hanks@gmail.com>
2014-03-23 17:24:23 -0700

    Intro section, images
    - Finished intro section
    - Added new images

--- Source/js/main.js
+++ Source/js/main.js
@@ -1,1 +1,24 @@
-﻿
+﻿// DEPENDENCIES: jQuery
+(function($, window, document)
+{
+	"use strict";
+
+	$(function() { _Init(); }); // jQuery ready event to kick off Init
+
+	var _Init = function()
+	{
+		/// <summary>Initializes the page.</summary>
+
+		if($("#intro").css("position") === "fixed")
+		{
+			var viewportHeight = $(window).height();
+			$("#intro").height(viewportHeight);
+			$("#body").css("margin-top", viewportHeight + "px");
+		}
+	};
+
+	var _MyPrivateFunc = function()
+	{
+
+	};
+})(window.jQuery, window, document);
