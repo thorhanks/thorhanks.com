e9976b4cf735c150fbe04915cc9e91ff9096db78
thor.hanks <thor.hanks@gmail.com>
2014-03-30 01:15:57 -0700

    Cleanup - Work section - misc
    - Cleanup and changed direction on some layout
    - Finished work section

--- Source/js/main.js
+++ Source/js/main.js
@@ -1,6 +1,8 @@
 ﻿// DEPENDENCIES: jQuery
 (function($, window, document)
 {
+	var introOriginalHeight;
+
 	"use strict";
 
 	$(function() { _Init(); }); // jQuery ready event to kick off Init
@@ -9,12 +11,14 @@
 	{
 		/// <summary>Initializes the page.</summary>
 
-		if($("#intro").css("position") === "fixed")
-		{
-			var viewportHeight = $(window).height();
-			$("#intro").height(viewportHeight);
-			$("#body").css("margin-top", viewportHeight + "px");
-		}
+		introOriginalHeight = $("#intro").height();
+
+		// Call once to init
+		_FitIntroSectionToWindow();
+
+		// Setup fit func to run on resize
+		$(window).on("resize", _FitIntroSectionToWindow);
+
 
 		$(window).scroll(function()
 		{
@@ -28,12 +32,44 @@
 		// History timeline bar on click handler
 		$("#about").find(".history .bar").on("click", function()
 		{
-			document.getElementById("about").className = this.attributes["data-id"].value;
+			document.getElementById("about").className = this.attributes["data-id"].value + " section";
+		});
+
+		// Project click handler
+		$("#work").find(".project").on("click", function(e)
+		{
+			var $target = $(e.target);
+
+			if(!$target.is("a"))
+				$(this).toggleClass("selected");
 		});
 	};
 
-	var _MyPrivateFunc = function()
+	var _FitIntroSectionToWindow = function()
 	{
+		/// <summary>Adjust intro section to fill the viewport height.</summary>
 
+		var contentHeight = $("#intro > div").height() + 100;
+		var viewportHeight = $(window).height();
+
+		// IF the window view is taller than the content then use it else keep content height
+		var newHeight = (viewportHeight > contentHeight) ? viewportHeight : contentHeight;
+		
+		$("#intro").height(newHeight);
+
+		// IF window is mobile width
+		if(_isMobile())
+			$("#body").css("margin-top", "0px");
+		else
+			$("#body").css("margin-top", newHeight + "px");
+	};
+
+	var _isMobile = function()
+	{
+		/// <summary>Returns true if window is currently set for mobile.</summary>
+
+		// IF Intro section is fixed it means media queries
+		// set it up for desktop width
+		return !($("#intro").css("position") === "fixed");
 	};
 })(window.jQuery, window, document);
